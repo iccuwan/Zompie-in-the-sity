39022ed82f29c2c4498366de744c9982a9bd3ef6
SexyTolik <48523152+SexyTolik@users.noreply.github.com>
2020-05-08 21:59:00 +0500

    Debug карта + 1 общий скрипт

---
+++ Assets/script/Units.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    public float HP;
+    public float MoveSpeed;
+    public float Damage;
+
+    void Update()
+    {
+        
+    }
+}
