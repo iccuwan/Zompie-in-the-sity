6ec6d5c845687a2c89a4c47e3ee314f5adb8152f
Mikhail <sovremen1@gmail.com>
2020-05-13 20:53:49 +0300

    Zombie optimization

--- Assets/Scripts/Units/Zombie.cs
+++ Assets/Scripts/Units/Zombie.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Zombie : Unit
 {
+    public float attackMoveDistance = 10f;
     public float attackDistance = 1f;
 
     private List<Spec> targetList = new List<Spec>();
@@ -17,13 +18,23 @@ public class Zombie : Unit
 
     void Update()
     {
+        Vector2 position = transform.position;
+        foreach (Spec s in GameManager.Instance.SpecList)
+        {
+            if (!targetList.Contains(s) && Vector2.Distance(position, s.transform.position) <= attackMoveDistance)
+            {
+                targetList.Add(s);
+                continue;
+            }
+            targetList.Remove(s);
+        }
         if (targetList.Count > 0)
         {
             Spec nearest = null;
             float nearestDistance = 0f;
             foreach (Spec s in targetList)
             {
-                float distance = Vector2.Distance(transform.position, s.transform.position);
+                float distance = Vector2.Distance(position, s.transform.position);
                 if (nearest != null)
                 {
                     if (nearestDistance > distance)
@@ -43,12 +54,12 @@ public class Zombie : Unit
         if (target != null)
         {
             // По хорошему это надо сделать через Unit.cs, как сделано для Spec
-            float rad = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x);
+            float rad = Mathf.Atan2(target.transform.position.y - position.y, target.transform.position.x - position.x);
             float deg = (180 / Mathf.PI) * rad;
-            if (Vector2.Distance(transform.position, goTo) > attackDistance)
+            if (Vector2.Distance(position, goTo) > attackDistance)
             {
                 unitStatus = UnitStatus.Walking;
-                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(position, target.transform.position, moveSpeed * Time.deltaTime);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, deg), rotationSpeed * Time.deltaTime);
             }
             else
@@ -60,27 +71,6 @@ public class Zombie : Unit
         else
         {
             unitStatus = UnitStatus.Idle;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Spec")
-        {
-            targetList.Add(collision.GetComponent<Spec>());
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Spec")
-        {
-            Spec s = collision.GetComponent<Spec>();
-            if (s == target)
-            {
-                target = null;
-            }
-            targetList.Remove(s);
         }
     }
 }
